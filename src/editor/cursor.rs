458b806182ef3869c216800580786500a39d780a
danielbartsch <daniel.bartsch@adverity.com>
2019-11-09 17:37:42 +0100

    implemented new test spec. See commit message from 96f43a0

--- src/editor/cursor.rs
+++ src/editor/cursor.rs
@@ -77,80 +77,79 @@ pub mod cursor {
             }
         }
         pub fn home(&mut self, select: bool) {
-            if self.current.column == 0
-                && self.line_lengths[self.current.line] >= self.current.column_offset
+            let moving_cursor = self.get_moving_cursor(select);
+            let move_to = if moving_cursor.column == 0
+                && self.line_lengths[moving_cursor.line] >= moving_cursor.column_offset
             {
-                self.current.column = self.current.column_offset;
-                if !select {
-                    self.extender.column = self.extender.column_offset;
-                }
+                moving_cursor.column_offset
             } else {
-                self.current.column = 0;
-                if !select {
-                    self.extender.column = 0;
-                }
+                0
+            };
+            self.extender.column = move_to;
+            if self.cursors_need_sync(select) {
+                self.current.column = move_to;
             }
         }
         pub fn end(&mut self, select: bool) {
-            let current_line_max_column = self.line_lengths[self.extender.line];
-            if self.extender.column == current_line_max_column
-                && current_line_max_column >= self.extender.column_offset
+            let moving_cursor = self.get_moving_cursor(select);
+            let current_line_max_column = self.line_lengths[moving_cursor.line];
+            let move_to = if moving_cursor.column == current_line_max_column
+                && current_line_max_column >= moving_cursor.column_offset
             {
-                if !select {
-                    self.current.column = self.current.column_offset;
-                }
-                self.extender.column = self.extender.column_offset;
+                moving_cursor.column_offset
             } else {
-                if !select {
-                    self.current.column = current_line_max_column;
-                }
-                self.extender.column = current_line_max_column;
+                current_line_max_column
+            };
+
+            self.extender.column = move_to;
+            if self.cursors_need_sync(select) {
+                self.current.column = move_to;
             }
         }
         pub fn left(&mut self, select: bool) {
-            let move_end = !select;
-            let (new_start_line, new_start_column) = if self.current.column == 0 {
-                if self.current.line != 0 {
-                    let previous_line = self.current.line - 1;
+            let moving_cursor = self.get_moving_cursor(select);
+            let (new_start_line, new_start_column) = if moving_cursor.column == 0 {
+                if moving_cursor.line != 0 {
+                    let previous_line = moving_cursor.line - 1;
                     (
                         previous_line,
                         *self.line_lengths.get(previous_line).unwrap(),
                     )
                 } else {
-                    (self.current.line, self.current.column)
+                    (moving_cursor.line, moving_cursor.column)
                 }
             } else {
-                (self.current.line, self.current.column - 1)
+                (moving_cursor.line, moving_cursor.column - 1)
             };
-            self.current.column = new_start_column;
-            self.current.line = new_start_line;
-            self.current.column_offset = self.current.column;
-            if move_end {
-                self.extender.column = new_start_column;
-                self.extender.line = new_start_line;
-                self.extender.column_offset = self.extender.column;
+            self.extender.column = new_start_column;
+            self.extender.line = new_start_line;
+            self.extender.column_offset = new_start_column;
+            if self.cursors_need_sync(select) {
+                self.current.column = new_start_column;
+                self.current.line = new_start_line;
+                self.current.column_offset = new_start_column;
             }
         }
         pub fn right(&mut self, select: bool) {
-            let move_start = !select;
-            let current_line_max_column = self.line_lengths.get(self.extender.line).unwrap();
-            let has_next_line = self.extender.line < self.line_lengths.len() - 1;
+            let moving_cursor = self.get_moving_cursor(select);
+            let current_line_max_column = self.line_lengths.get(moving_cursor.line).unwrap();
+            let has_next_line = moving_cursor.line < self.line_lengths.len() - 1;
             let (new_end_line, new_end_column) =
-                if &self.extender.column == current_line_max_column && has_next_line {
-                    (self.extender.line + 1, 0)
-                } else if &self.extender.column < current_line_max_column {
-                    (self.extender.line, self.extender.column + 1)
+                if &moving_cursor.column == current_line_max_column && has_next_line {
+                    (moving_cursor.line + 1, 0)
+                } else if &moving_cursor.column < current_line_max_column {
+                    (moving_cursor.line, moving_cursor.column + 1)
                 } else {
-                    (self.extender.line, self.extender.column)
+                    (moving_cursor.line, moving_cursor.column)
                 };
-            if move_start {
+            self.extender.line = new_end_line;
+            self.extender.column = new_end_column;
+            self.extender.column_offset = new_end_column;
+            if self.cursors_need_sync(select) {
                 self.current.line = new_end_line;
                 self.current.column = new_end_column;
                 self.current.column_offset = new_end_column;
             }
-            self.extender.line = new_end_line;
-            self.extender.column = new_end_column;
-            self.extender.column_offset = new_end_column;
         }
         pub fn up(&mut self, select: bool) {
             self.vertical(-1, select);
@@ -160,83 +159,66 @@ pub mod cursor {
         }
 
         fn vertical(&mut self, direction: isize, select: bool) {
-            let move_start = !select || direction < 0;
-            let move_end = !select || direction > 0;
-            self.vertical_movement_line(direction, select, move_start, move_end);
-            self.vertical_movement_column(direction, select, move_start, move_end);
+            self.vertical_movement_line(direction, select);
+            self.vertical_movement_column(select);
         }
-        fn vertical_movement_line(
-            &mut self,
-            direction: isize,
-            select: bool,
-            move_start: bool,
-            move_end: bool,
-        ) {
-            let next_move_to_line = if direction < 0 {
-                self.current.line as isize
+        fn vertical_movement_line(&mut self, direction: isize, select: bool) {
+            let moving_cursor = self.get_moving_cursor(select);
+
+            let next_move_to_line = moving_cursor.line as isize + direction;
+            let new_line = if next_move_to_line > (self.line_lengths.len() - 1) as isize {
+                self.line_lengths.len() - 1
+            } else if next_move_to_line <= 0 {
+                0
+            } else if !select && moving_cursor.line != moving_cursor.line {
+                next_move_to_line as usize
             } else {
-                self.extender.line as isize
-            } + direction;
-            let (new_start_line, new_end_line) =
-                if next_move_to_line > (self.line_lengths.len() - 1) as isize {
-                    (self.line_lengths.len() - 1, self.line_lengths.len() - 1)
-                } else if next_move_to_line <= 0 {
-                    (0, 0)
-                } else if !select && self.current.line != self.extender.line {
-                    (next_move_to_line as usize, next_move_to_line as usize)
-                } else {
-                    (
-                        ((self.current.line as isize) + direction) as usize,
-                        ((self.extender.line as isize) + direction) as usize,
-                    )
-                };
-            if move_start {
-                self.current.line = new_start_line;
-            }
-            if move_end {
-                self.extender.line = new_end_line;
+                ((moving_cursor.line as isize) + direction) as usize
+            };
+            self.extender.line = new_line;
+            if self.cursors_need_sync(select) {
+                self.current.line = new_line;
             }
         }
 
-        fn vertical_movement_column(
-            &mut self,
-            direction: isize,
-            select: bool,
-            move_start: bool,
-            move_end: bool,
-        ) {
-            let moving_cursor_part = if direction < 0 {
-                &self.current
-            } else {
-                &self.extender
-            };
-            let next_line_max_column = self.line_lengths.get(moving_cursor_part.line).unwrap();
-            let (new_start_column, new_end_column) =
-                if !select && self.current.column != self.extender.column {
-                    let actual_new_column = if &moving_cursor_part.column > next_line_max_column {
-                        next_line_max_column
-                    } else {
-                        &moving_cursor_part.column
-                    };
-                    (*actual_new_column, *actual_new_column)
-                } else if &self.current.column > next_line_max_column
-                    || &self.current.column < &self.current.column_offset
-                        && &self.current.column < next_line_max_column
-                        && &self.current.column_offset > next_line_max_column
-                {
-                    (*next_line_max_column, *next_line_max_column)
-                } else if &self.current.column < &self.current.column_offset
-                    && &self.current.column < next_line_max_column
-                {
-                    (self.current.column_offset, self.extender.column_offset)
+        fn vertical_movement_column(&mut self, select: bool) {
+            let moving_cursor = self.get_moving_cursor(select);
+
+            let next_line_max_column = self.line_lengths.get(moving_cursor.line).unwrap();
+            let new_column = if !select && moving_cursor.column != moving_cursor.column {
+                let actual_new_column = if &moving_cursor.column > next_line_max_column {
+                    next_line_max_column
                 } else {
-                    ((self.current.column, self.extender.column))
+                    &moving_cursor.column
                 };
-            if move_start {
-                self.current.column = new_start_column;
+                *actual_new_column
+            } else if &moving_cursor.column > next_line_max_column
+                || &moving_cursor.column < &moving_cursor.column_offset
+                    && &moving_cursor.column < next_line_max_column
+                    && &moving_cursor.column_offset > next_line_max_column
+            {
+                *next_line_max_column
+            } else if &moving_cursor.column < &moving_cursor.column_offset
+                && &moving_cursor.column < next_line_max_column
+            {
+                moving_cursor.column_offset
+            } else {
+                moving_cursor.column
+            };
+            self.extender.column = new_column;
+            if self.cursors_need_sync(select) {
+                self.current.column = new_column;
             }
-            if move_end {
-                self.extender.column = new_end_column;
+        }
+
+        fn cursors_need_sync(&self, select: bool) -> bool {
+            !select && self.current != self.extender
+        }
+        fn get_moving_cursor(&self, select: bool) -> &CursorPosition {
+            if !select || self.current != self.extender {
+                &self.extender
+            } else {
+                &self.current
             }
         }
     }
